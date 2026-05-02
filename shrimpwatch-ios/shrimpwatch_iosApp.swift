4afbb7e6b65b4b37542edb03e14bee7d0583aa82
fianpraniza <fianpraniza@gmail.com>
2026-05-02 15:34:28 +0700

    Initial Commit

---
+++ shrimpwatch-ios/shrimpwatch_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  shrimpwatch_iosApp.swift
+//  shrimpwatch-ios
+//
+//  Created by Muhammad Arfian Praniza on 02/05/26.
+//
+
+import SwiftUI
+
+@main
+struct shrimpwatch_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
