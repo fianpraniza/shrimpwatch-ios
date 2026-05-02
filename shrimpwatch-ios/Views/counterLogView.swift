78afdd6cbbaa886c2b6c9a32f505d0ce01c17f30
fianpraniza <fianpraniza@gmail.com>
2026-05-02 18:40:16 +0700

    home v1

---
+++ shrimpwatch-ios/Views/counterLogView.swift
@@ -0,0 +1,18 @@
+//
+//   counterLogView.swift
+//  shrimpwatch-ios
+//
+//  Created by Muhammad Arfian Praniza on 02/05/26.
+//
+
+import SwiftUI
+
+struct counterLogView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    counterLogView()
+}
--- shrimpwatch-ios/shrimpwatch_iosApp.swift
+++ shrimpwatch-ios/shrimpwatch_iosApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct shrimpwatch_iosApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack {
+                homeView()
+            }
         }
     }
 }
